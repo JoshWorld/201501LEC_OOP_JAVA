5617af8c9fb96e0f7fac5c7804d16f1dd91211c9
dknife <young.min.kang@gmail.com>
2015-06-03 11:37:22 +0900

    thread info

--- 12_2_ThreadInfo/src/ThreadInfo.java
+++ 12_2_ThreadInfo/src/ThreadInfo.java
@@ -25,15 +25,24 @@ class TimerThread implements Runnable {
 	}
 }
 
-public class ThreadTimer extends JFrame {
+public class ThreadInfo extends JFrame {
 	
 	JLabel textMsg = new JLabel("Use Arrow Keys to Move Me");
+	JLabel[] threadInfo = new JLabel[4];
+	String[] infoString = { "Id: ", "Name: ", "Priority: ", "State: "};
 	
-	public ThreadTimer() {
+	public ThreadInfo() {
 		setTitle("Thread Timer");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container c = getContentPane();
 		c.setLayout(null);
+		
+		for(int i=0;i<threadInfo.length;i++) {
+			threadInfo[i] = new JLabel(infoString[i]);
+			threadInfo[i].setLocation(0, 20+20*i);
+			threadInfo[i].setSize(200, 20);
+			c.add(threadInfo[i]);
+		}
 		
 		textMsg.setLocation(500,300);
 		textMsg.setSize(260,20);
@@ -48,13 +57,29 @@ public class ThreadTimer extends JFrame {
 		
 		Thread th = new Thread(new TimerThread(timerLabel));
 		
-		
 		setSize(1024,768);
 		setVisible(true);
 		
 		c.addKeyListener(new MyKeyListener());
 		c.requestFocus();
 		th.start();
+		
+		long threadId = th.getId();
+		infoString[0] = infoString[0] + Long.toString(threadId);
+		threadInfo[0].setText(infoString[0]);
+		
+		String threadName = th.getName();
+		infoString[1] = infoString[1] + threadName;
+		threadInfo[1].setText(infoString[1]);
+		
+		int threadPriority = th.getPriority();
+		infoString[2] = infoString[2] + Integer.toString(threadPriority);
+		threadInfo[2].setText(infoString[2]);
+		
+		Thread.State threadState = th.getState();
+		infoString[3] = infoString[3] + threadState.toString();
+		threadInfo[3].setText(infoString[3]);
+		
 	}
 	class MyKeyListener extends KeyAdapter {
 		public void keyPressed(KeyEvent e) {
@@ -76,6 +101,6 @@ public class ThreadTimer extends JFrame {
 	}
 	
 	public static void main(String[] args) {
-		new ThreadTimer();
+		new ThreadInfo();
 	}
 }
